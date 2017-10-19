b93adc23df8a70fd0f44fd2676116f7fb950d5ea
wfe-luigi <kdh3979@naver.com>
2017-10-19 11:47:24 +0900

    변수 추가 및 switch-case 교체

--- grade3_2.js
+++ grade3_2.js
@@ -1,26 +1,9 @@
 // 총평점 : 3.92, 전공평점:3.5, 이수학점:12, 전공이수학점:6.
 function getScore(grade){
-    switch(grade){
-        case 'A+': return 4.5;
-        break;
-        case 'A' : return 4.0;
-        break;
-        case 'B+' : return 3.5;
-        break;
-        case 'B' : return 3.0;
-        break;
-        case 'C+' : return 2.5;
-        break;
-        case 'C' : return 2.0;
-        break;
-        case 'D+' : return 1.5;
-        break;
-        case 'D' : return 1.0;
-        break;
-        default : return 0;
-    }
+    var sum = "FDCBA".indexOf(grade[0]);
+    if(grade[1] == '+') sum += 0.5;
+    return sum;
 }
-
 let data =  [ 
     {
         'name' : '데이터베이스', 
@@ -46,7 +29,8 @@ function addLecture (score){
     printScore(data);
 }
 function printScore(score){
-    let grade_sum = 0, grade_done = 0, major_sum = 0, major_done = 0;
+    var grade_sum = 0, grade_done = 0, major_sum = 0, major_done = 0;
+    var total, major_avg, changeScore;
     score.forEach(
         function(task){
             grade_done +=  task.credit;
@@ -57,15 +41,15 @@ function printScore(score){
             }
         }
     );
-    
-    console.log("총평점 : " + (grade_sum/grade_done).toFixed(2) 
-    + ", 전공 평점 : "+ (major_sum/major_done).toFixed(2) 
+    total = (grade_sum/grade_done).toFixed(2);
+    major_avg = (major_sum/major_done).toFixed(2);
+    changeScore = ((grade_sum/grade_done)*4/4.5).toFixed(2);
+
+    console.log("총평점 : " + total
+    + ", 전공 평점 : "+ major_avg 
     + ", 이수학점 : " + grade_done
     + ", 전공이수학점 : " + major_done);
-
-    //요구사항2 4.5가 아니고 4.0 만점의 경우 총평점
-    console.log("4.0학점으로 변환하는 경우 총평점은 " + ((grade_sum/grade_done)*4/4.5).toFixed(2) + "입니다.")
-    
+    console.log("4.0학점으로 변환하는 경우 총평점은 " + changeScore + "입니다.")
 }
 printScore(data); // 예상결과 3.92 4 6 3
 addLecture({'name' : '알고리즘', 'grade' : 'B', 'credit' : 3, 'bMajor' : true}); //예상결과 3.61 3.5 9 6 3.21
