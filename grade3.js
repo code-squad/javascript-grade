5126cef3f45db6735c433b198975c5b47d7d4dec
Dongwoo Lee <likedemian@me.com>
2018-01-09 22:57:58 +0900

    [dongdong] Add: 전공 항목 추가 및 새로운 함수 제작

--- grade3.js
+++ grade3.js
@@ -1,17 +1,20 @@
 var DATA = [{
     'name': '데이터베이스',
     'grade': 'A',
-    'credit': 3
+    'credit': 3,
+    'bMajor': true
   },
   {
     'name': '교양영어',
     'grade': 'B+',
-    'credit': 2
+    'credit': 2,
+    'bMajor': false
   },
   {
     'name': '철학',
     'grade': 'B+',
-    'credit': 1
+    'credit': 1,
+    'bMajor': false
   }
 ]
 
@@ -31,11 +34,11 @@ var GP = {
 
 // 총 이수학점을 구하는 함수
 function sumCredit(obj) {
-  var creditSum = 0;
+  var sumCredit = 0;
   obj.map(value => {
-    creditSum += value.credit;
+    sumCredit += value.credit;
   });
-  return creditSum
+  return sumCredit
 }
 
 
@@ -47,9 +50,71 @@ function averageCredit(obj) {
     var lectures = obj[point];
     gradePoints += GP[lectures.grade] * lectures.credit;
   }
-  return (gradePoints / sumCredit(DATA)).toFixed(2)
+  return (gradePoints / sumCredit(DATA)).toFixed(2);
 }
 
 
-console.log("이수학점:", sumCredit(DATA));
-console.log("평균:", averageCredit(DATA));
+// 4.0 만점일 경우 평점을 구하는 함수
+function averageChangeCredit(obj) {
+  var gradePoints = 0;
+  for (var point in obj) {
+    var lectures = obj[point];
+    gradePoints += GP[lectures.grade] * lectures.credit;
+  }
+  return ((gradePoints * 4.0 / 4.5) / sumCredit(DATA)).toFixed(2);
+}
+
+
+
+// 전공 평점을 구하는 함수
+function averageCreditsByMajor(obj) {
+  var gradeMajorPoints = 0;
+  for (var key in obj) {
+    var lectures = obj[key]
+    if (lectures.bMajor) {
+      gradeMajorPoints += GP[lectures.grade] * lectures.credit;
+    };
+  }
+  return (gradeMajorPoints / sumCreditByMajor(DATA)).toFixed(2);
+}
+
+
+
+// 전공 이수학점을 구하는 함수
+function sumCreditByMajor(obj) {
+  var sumCredit = 0;
+  obj.forEach(elem => {
+    if (elem.bMajor) {
+      sumCredit += elem.credit
+    }
+  });
+  return sumCredit
+}
+
+
+
+// 학점계산에 필요한 함수들을 모아 console.log로 출력하는 함수
+function calculateGPA(obj) {
+  console.log("총평점: ", averageCredit(obj));
+  console.log("전공평점: ", averageCreditsByMajor(obj));
+  console.log("이수학점: ", sumCredit(obj));
+  console.log("전공이수학점: ", sumCreditByMajor(obj));
+  console.log("4.0학점으로 변환하는 경우 총 평점은 " + averageChangeCredit(obj) + "입니다.");
+}
+
+
+
+// 새로운 과목을 추가하는 함수 
+function addLecture(subject) {
+  DATA.push(subject);
+  return calculateGPA(DATA)
+}
+
+
+
+addLecture({
+  'name': '알고리즘',
+  'grade': 'B+',
+  'credit': 3,
+  'bMajor': true
+});
