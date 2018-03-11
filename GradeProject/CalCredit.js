8ef006cd17a8303e3b17af76b8d429b06b2441d1
HYEON(win) <antaehyeon@live.co.kr>
2018-03-11 20:53:32 +0900

    [Add] '학점계산기' 중 '평점과 이수학점' 요구사항1에 맞춘 코드를 추가한다
    요구사항은 '전공항목 추가'입니다. 총평점과 이수학점 외에 전공평점과 전공이수학점을 추가로
    출력할 수 있게 하기 위해서, 변수 네이밍과 조건문을 추가하였습니다

--- GradeProject/CalCredit.js
+++ GradeProject/CalCredit.js
@@ -2,28 +2,41 @@ function showGrade(data) {
     // 일단 과목의 갯수를 체크하기 위해서 변수를 선언
     // 이수학점 데이터를 관리하기 위해 변수 선언
     // 총 평점 데이터를 관리하기 위해 변수 선언
+    // 전공평점 데이터를 관리하기 위해 변수 선언
     var subjectNumber = data.length;
-    var completionGradeNum = 0;
-    var totalGradeNum = 0;
 
-    const gradeIndex = 1;
-    const gradeNumIndex = 2;
+    var totalGradeNum = 0; // 총 평점
+    var majorGradeNum = 0; // 전공 평점
+
+    var completionCredit = 0; // 이수 학점
+    var majorCompletionCredit = 0; // 전공 이수 학점
 
     // 과목의 갯수만큼 반복문을 수행
     for (obj in data) {
         // 해당 데이터에서 인덱스를 접근해 데이터 계산
         var grade = data[obj].grade;
-        var gradeNum = data[obj].credit;
+        var credit = data[obj].credit;
+        var major = data[obj].major;
 
-        completionGradeNum += translateGradeToNum(grade);
-        totalGradeNum += gradeNum;
+        totalGradeNum += translateGradeToNum(grade);
+        completionCredit += credit;
+
+        // 전공이라면
+        // 전공평점과 전공이수학점에 계산
+        if (major) {
+            majorGradeNum += translateGradeToNum(grade);
+            majorCompletionCredit += credit;
+        }
     }
 
     // 소숫점 3번째 자리에서 반올림 처리
-    completionGradeNum = (completionGradeNum/subjectNumber).toFixed(2);
+    totalGradeNum = (totalGradeNum/subjectNumber).toFixed(2);
 
     // 출력
-    console.log("총 평점 " + completionGradeNum + " , 이수학점 " + totalGradeNum);
+    console.log("총 평점 " + totalGradeNum +
+                " , 전공평점 : " + majorGradeNum +
+                " , 이수학점 " + completionCredit +
+                " , 전공이수학점 : " + majorCompletionCredit);
 }
 
 // 학점(A+, A ...) 을 숫자로 변환하기 위한 함수
@@ -51,21 +64,25 @@ function translateGradeToNum(_grade) {
 }
 
 // 객체(딕셔너리) 버전
+// 전공항목 추가
 var data =  [
     {
         'name' : '데이터베이스',
         'grade' : 'A',
-        'credit' : 3
+        'credit' : 3,
+        'major' : true
     },
     {
         'name' : '교양영어',
         'grade' : 'B+',
-        'credit' : 2
+        'credit' : 2,
+        'major' : false
     },
     {
         'name' : '철학',
         'grade' : 'B+',
-        'credit' : 1
+        'credit' : 1,
+        'major' : false
     }
 ];
 showGrade(data);
