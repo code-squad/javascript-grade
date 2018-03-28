b0c6af092f322b6be95e5c57692ffe921bbd81bf
feanar <feanar729@gmail.com>
2018-03-28 16:40:12 +0900

    refactoring & updata: 기능 refactoring, 변수명 변경 및 객체로 성적바꾸기 lesson 완료

--- grade.js
+++ grade.js
@@ -1,6 +1,3 @@
-// refatoring 
-// 1. 이중 for문을 대체할 기능 구현
-// 2. 성적탐색, 이수학점 탐색 기능 중 중복 된 기능 합치기 => 탐색기능(?)
 var data = [
   {
     'name': '데이터베이스',
@@ -31,46 +28,17 @@ var grade = { // 해당성적점수
   "F": 0
 };
 
-var totalCredit = 0; // 총이수학점
-var totalCalculate = 0; // 총계산값(이수학점*과목점수..)
-var totalAvg = 0; // 총평균점수
-
-// 성적 탐색 합계 함수
-function searchGrade(inputData) {
-  let data = inputData;
-  let result = [];
-  for (var key in data) {
-    for (var value in grade) {
-      if (value === data[key][1]) {
-        result.push(grade[value]);
-      }
-    }
-  }
-  return result;
-}
-
-// 이수학점 탐색 합계 함수
-function searchData(data) {
-  let eduCredit = [];
-  for (var credit in data) {
-    if (typeof data[credit][2] === "number") {
-      eduCredit.push(data[credit][2]);
-    }
-  }
-  return eduCredit;
-}
-
-// 총 평점 & 총이수학점 계산
 function showGrade(data) {
-  let eduCredit = searchData(data);
-  let resultGrade = searchGrade(data);
-  let totalCredit = eduCredit.reduce(function(preKey, lastKey) { return preKey + lastKey; });  
-  for(var i = 0; i < resultGrade.length; i++){
-    totalCalculate += resultGrade[i] * eduCredit[i];
-  }
-  let totalAvg = (totalCalculate / totalCredit).toFixed(2); 
+  let totalAvg = 0; // 총 평점
+  let sumGrade = 0; // 해당성적*이수학점의 합
+  let sumCredit = 0; // 총 이수학점
   
-  return "총평점: " + totalAvg + ", " + "이수학점: " + totalCredit;
+  for (let i = 0; i < data.length; i++) {
+    sumCredit += data[i].credit;
+    sumGrade += grade[data[i].grade] * data[i].credit;
+  }
+  totalAvg = (sumGrade / sumCredit).toFixed(2); 
+  return "총평점: " + totalAvg + ", " + "이수학점: " + sumCredit;
 }
 
-console.log(showGrade(data)); // "총평점 3.92 , 이수학점 6"
+console.log(showGrade(data));
