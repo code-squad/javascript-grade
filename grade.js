b0a2ba2255d77815fd2001bb62a8a9237f114231
feanar <feanar729@gmail.com>
2018-04-03 15:23:27 +0900

    update & refactoring: 전공 데이터 탐색 함수 제작/ 성적 계산,성적 출력 함수 변수명 수정 및 기능 refactoring

--- grade.js
+++ grade.js
@@ -1,3 +1,4 @@
+
 var data = [
   {
     'name': '데이터베이스',
@@ -35,82 +36,70 @@ var grade = { // 해당성적점수
   "D+": 1.5,
   "D": 1.0,
   "F": 0
-  
+
 };
 
-// 전공 과목 성적 계산
-var getCalculatedMajorGrade = function(data){
-  let sumMajorGrade = 0; // 전공 성적 * 이수학점 합
-  let sumMajorCredit = 0; // 총 전공 이수학점
-  let calculatedMajorValue = {
-    "sumMajorGrade": [], 
-    "sumMajorCredit": [], 
-  };
-
+// 전공만 선택(골라내는)
+function getSelectMajor(data) {
+  let result = [];
   for (let i = 0; i < data.length; i++) {
-    if (data[i].major) {
-      sumMajorCredit += data[i].credit;
-      sumMajorGrade += grade[data[i].grade] * data[i].credit;
-    }
+    if (!!data[i].major) result.push(data[i]);
   }
-
-  calculatedMajorValue.sumMajorGrade.push(sumMajorGrade), 
-  calculatedMajorValue.sumMajorCredit.push(sumMajorCredit);
-
-  return calculatedMajorValue;
-};
+  return result;
+}
 
 // 과목 성적 계산
-function getCalculatedGrade(data){
+function getCalculatedGrade(data) {
   let sumGrade = 0; // 해당성적 * 이수학점의 합 계산(전공/전공+나머지)
   let sumCredit = 0; // 이수학점 계산                         
   let calculatedValue = {
-    "sumGrade": [],
-    "sumCredit": []
+    "sumGradeValue": [],
+    "sumCreditValue": []
   };
-  
+
   for (let i = 0; i < data.length; i++) {
     sumCredit += data[i].credit;
-    sumGrade += grade[data[i].grade] * data[i].credit; 
+    sumGrade += grade[data[i].grade] * data[i].credit;
   }
-  calculatedValue.sumGrade.push(sumGrade);
-  calculatedValue.sumCredit.push(sumCredit);
-  
+  calculatedValue.sumGradeValue.push(sumGrade);
+  calculatedValue.sumCreditValue.push(sumCredit);
+
   return calculatedValue;
 }
 
 // 전 과목 성적 출력
-function getAllGrade(data){
+function getAllGrade(data) {
   let calculatedValue = getCalculatedGrade(data); // 전과목 계산 함수
-  let calculatedMajorValue = getCalculatedMajorGrade(data); // 전공과목 계산 함수 
-  
-  let majorGrade = calculatedMajorValue.sumMajorGrade;
-  let majorCredit = calculatedMajorValue.sumMajorCredit;
-  
-  let allGrade = calculatedValue.sumGrade; 
-  let allCredit = calculatedValue.sumCredit;
-  
+  let calculatedMajorValue = getCalculatedGrade(getSelectMajor(data));
+
+  // 전공 결과값
+  let majorGrade = calculatedMajorValue.sumGradeValue;
+  let majorCredit = calculatedMajorValue.sumCreditValue;
+
+  // 전공+나머지 결과값
+  let allGrade = calculatedValue.sumGradeValue;
+  let allCredit = calculatedValue.sumCreditValue;
+
   let totalAvg = (allGrade / allCredit).toFixed(2);
   let totalAvgMajor = (majorGrade / majorCredit).toFixed(2);
   let transAvg = (totalAvg * 8 / 9).toFixed(2);
-  
+
   let printAllGrade = "총평점: " + totalAvg + ", " + "이수학점: " + allCredit + ", ";
-  let printAllMajor = "전공평점: " + totalAvgMajor + ", " + "전공이수학점: " + majorCredit +"\n";
-  let printTransAVG = "4.0 학점으로 변환되는 경우 총평점은 " + transAvg + "입니다."; 
-  let printFinAVG = printAllGrade + printAllMajor + printTransAVG; 
-  
+  let printAllMajor = "전공평점: " + totalAvgMajor + ", " + "전공이수학점: " + majorCredit + "\n";
+  let printTransAVG = "4.0 학점으로 변환되는 경우 총평점은 " + transAvg + "입니다.";
+  let printFinAVG = printAllGrade + printAllMajor + printTransAVG;
+
   return printFinAVG;
 }
 
 // 과목 추가 함수
 function addLecture(addData) {
-  if(typeof addData === "object"){
+  if (typeof addData === "object") {
     return data.push(addData);
   }
 }
 
-
-addLecture({'name': '알고리즘', 'grade': 'B', 'credit': 3, 'major': true});
+addLecture({ 'name': '알고리즘', 'grade': 'B', 'credit': 3, 'major': true });
 
 var allGrade = getAllGrade(data);
 console.log(allGrade);
