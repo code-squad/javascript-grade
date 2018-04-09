99f8b2688b32d53a0ed2306d457df7a374b239f3
feanar <feanar729@gmail.com>
2018-04-09 19:21:20 +0900

    refactoring & feedback: 과목추가 함수 조건문 Object.is 이용 refactoring 진행

--- grade.js
+++ grade.js
@@ -94,7 +94,7 @@ function getAllGrade(data) {
 
 // 과목 추가 함수
 function addLecture(addData) {
-  if (typeof addData === "object") {
+  if (Object.is(addData, addData)) {
     return data.push(addData);
   }
 }
