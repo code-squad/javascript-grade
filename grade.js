a4caf9f95c1dc1ab7f566abd1c107fa2cf65f36c
Kim <a01088570194@gmail.com>
2018-09-17 18:45:15 +0900

    Add declareData Function

--- grade.js
+++ grade.js
@@ -1,41 +1,51 @@
 // 총 학점 데이터
 
-function parseData(){
-    const data =  [ 
-        {
-            'name' : '데이터베이스', 
-            'grade' : 'A', 
-            'credit' : 3,
-            'major' : false
-        },
-        {
-            'name' : '교양영어', 
-            'grade' : 'B+', 
-            'credit' : 2,
-            'major' : true
-        },
-        {
-            'name' : '철학', 
-            'grade' : 'B+', 
-            'credit' : 1,
-            'major' : false
-        }
-    ];   
-
-    for(let value of data){
-        if(value.grade === 'A+') value.grade = 4.5;
-        if(value.grade === 'A') value.grade = 4.0;
-        if(value.grade === 'B+') value.grade = 3.5;
-        if(value.grade === 'B') value.grade = 3.0;
-        if(value.grade === 'C+') value.grade = 2.5;
-        if(value.grade === 'C') value.grade = 2.0;
-        if(value.grade === 'D+') value.grade = 1.5;
-        if(value.grade === 'D') value.grade = 1.0;
-        if(value.grade === 'F') value.grade = 0;
+const data =  [ 
+    {
+        'name' : '데이터베이스', 
+        'grade' : 'A', 
+        'credit' : 3,
+        'major' : false
+    },
+    {
+        'name' : '교양영어', 
+        'grade' : 'B+', 
+        'credit' : 2,
+        'major' : true
+    },
+    {
+        'name' : '철학', 
+        'grade' : 'B+', 
+        'credit' : 1,
+        'major' : false
     }
+];   
+
+// 데이터 변환(4.5 만점 기준)
+
+function parseData(data){
+    // 4.5기준 학점
+
+    const parse_data =  {
+            'A+': 4.5, 
+            'A' : 4, 
+            'B+': 3.5,
+            'B' : 3,
+            'C+': 2.5, 
+            'C' : 2, 
+            'D+': 1.5,
+            'D' : 1,
+            'F' : 0,
+    };
+
+    Object.keys(parse_data).forEach((v) =>{
+        for(let value of data){
+            if(value.grade === v) value.grade = parse_data[v];
+        }
+    })
+
     return data;
 }
-
 // 결과 메세지
 
 function showGrade(data){
@@ -59,8 +69,7 @@ function exchangeGrades(conversionNumber, grade){
 
 function checkTotalCreditNumber(data){
     let total_credit = 0;
-    
-    data.forEach(function(v){
+    data.forEach((v) => {
         total_credit += v.credit;
     })
     
@@ -71,9 +80,10 @@ function checkTotalCreditNumber(data){
 
 function checkMajorCreditNumber(data){
     let major_credit = 0;
+    let major_grades = data.filter((data) => {return !!(data.major)});
 
-    data.forEach(function(v){
-        if(v.major) major_credit += v.credit;
+    major_grades.forEach((v) => {
+        major_credit += v.credit;
     })
 
     return major_credit;
@@ -85,7 +95,7 @@ function calculateTotalCredit(data){
     let total_score = 0;
     const total_grades = checkTotalCreditNumber(data);
     
-    data.forEach(function(v){
+    data.forEach((v) => {
         total_score += v.credit * v.grade;
     })
     
@@ -96,15 +106,14 @@ function calculateTotalCredit(data){
 
 function calculateMajorCredit(data){
     let major_score = 0;
-    const major_grades = checkMajorCreditNumber(data);
+    let major_grades = data.filter((data) => {return !!(data.major)});
+    const calculation_major_grades = checkMajorCreditNumber(data);
     
-    data.forEach(function(v){
-        if(v.major) {
+    major_grades.forEach((v) => {
             major_score += v.credit * v.grade;
-        }
     })
 
-    return major_score/major_grades;
+    return major_score/calculation_major_grades;
 }
 
-showGrade(parseData());
+showGrade(parseData(data));
