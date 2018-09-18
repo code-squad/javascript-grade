a53d189f819d1e5dc087e128a02df3650bd8ae95
Head <mrdoo309@gmail.com>
2018-09-18 17:12:12 +0900

    delete useless function(print function, convertGradeAverage function), revise calcGradeAverage function(distinguish scoreStandard and calculategradeAverage)

--- calcGrade1.js
+++ calcGrade1.js
@@ -31,22 +31,11 @@ const gradeScoreObject = {
     'F':0
 }
 
-//학점평균을 소수 두번째 자리까지 구하는 함수
-function calcGradeAverage(totalGrade, totalCredit) {
-    return (totalGrade / totalCredit).toFixed(2)
+//학점평균을 점수기준을 받아 소수 두번째 자리까지 구하는 함수
+function calcGradeAverage(totalGrade, totalCredit, scoreStandard) {
+    return scoreStandard === 4.5 ? (totalGrade / totalCredit).toFixed(2) : ((totalGrade / totalCredit) * scoreStandard / 4.5).toFixed(2)
 }
 
-//학점평균을 인자로 입력받아 만점이4.0일때의 학점평균(소수 두번째 자리까지)으로 바꾸어주는 함수
-function convertGradeAverage(gradeAverage) {
-    return (gradeAverage * 4.0 / 4.5).toFixed(2)
-}
-//출력해주는 함수
-function printGrade(gradeAverage, majorGradeAverage, credit, majorCredit) {
-    console.log('4.5기준 총평점 : ' + gradeAverage + '(4.0기준은 ' + convertGradeAverage(gradeAverage) + ')');
-    console.log('4.5기준 전공평점 : ' + majorGradeAverage + '(4.0기준은 ' + convertGradeAverage(majorGradeAverage) + ')');
-    console.log('이수학점 : ' + credit);
-    console.log('전공이수학점 : ' + majorCredit);
-}
 //Data를 반영해 학점평균을 구하기위한 값들과 학점을 구하는 함수
 function showGrade(gradeData) {
     let totalGrade = 0
@@ -68,10 +57,14 @@ function showGrade(gradeData) {
             }
         }
     });
-    
-    const gradeAverage = calcGradeAverage(totalGrade, totalCredit)
-    const majorGradeAverage = calcGradeAverage(totalMajorGrade, totalMajorCredit)
-    printGrade(gradeAverage, majorGradeAverage, totalCredit, totalMajorCredit);
+    const gradeAverage = calcGradeAverage(totalGrade, totalCredit, 4.5)
+    const majorGradeAverage = calcGradeAverage(totalMajorGrade, totalMajorCredit, 4.5)
+    const convertedGradeAverage = calcGradeAverage(totalGrade, totalCredit, 4.0)
+    const convertedMajorGradeAverage = calcGradeAverage(totalMajorGrade, totalMajorCredit, 4.0)
+    console.log('4.5기준 총평점 : ' + gradeAverage + '(4.0기준은 ' + convertedGradeAverage + ')');
+    console.log('4.5기준 전공평점 : ' + majorGradeAverage + '(4.0기준은 ' + convertedMajorGradeAverage + ')');
+    console.log('이수학점 : ' + totalCredit);
+    console.log('전공이수학점 : ' + totalMajorCredit);
 }
 
 showGrade(data);
