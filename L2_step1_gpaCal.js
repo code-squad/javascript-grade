8d501a811a4756a62a168332a60f3eb14009812f
developersoom <developer.soom@gmail.com>
2018-09-18 17:19:00 +0900

    Update function - calculatePoints()
    created a  function for calculating points
    combined two cases (gross points & major subject points)

--- L2_step1_gpaCal.js
+++ L2_step1_gpaCal.js
@@ -30,3 +30,37 @@ function calculateCredits(data) {
     }
     return [credits, majorCredits];
 }
+
+// calculate points with 4.0 scale
+function calculatePoints(data) {
+    let grossPoints = 0;
+    let grossMajorPoints = 0;
+    tableFourPointZero = {
+        'A+': 4.3,
+        'A': 4.0,
+        'A-': 3.7,
+        'B+': 3.5,
+        'B': 3.0,
+        'B-': 2.7,
+        'C+': 2.5,
+        'C': 2.0,
+        'C-': 1.7,
+        'D+': 1.5,
+        'D': 1.0,
+        'D-': 0.7,
+        'F': 0,
+    }
+
+    for (let i in data) {
+        for (let key in data[i]) {
+            if (key === 'grade') {
+                grossPoints += (data[i].credit * tableFourPointZero[data[i][key]]);
+                if (data[i].major) {
+                grossMajorPoints += data[i].credit * tableFourPointZero[data[i][key]];
+                }
+            }
+
+        }
+    }
+    return [grossPoints, grossMajorPoints];
+}
