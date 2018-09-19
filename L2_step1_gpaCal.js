dfc1365635c15c7fdaa601395f6184c5c95340db
developersoom <developer.soom@gmail.com>
2018-09-19 17:00:43 +0900

    Modify calculatePoints() to avoid for-for

--- L2_step1_gpaCal.js
+++ L2_step1_gpaCal.js
@@ -46,40 +46,39 @@ function calculatePoints(data) {
     }
 
     for (const i in data) {
-        for (const key in data[i]) {
-            if (key === 'grade') {
-                grossPoints += (data[i].credit * fourPointFiveTable[data[i][key]]);
-                if (data[i].major) {
-                grossMajorPoints += data[i].credit * fourPointFiveTable[data[i][key]];
-                }
-            }
-
+        grossPoints += (data[i].credit * fourPointFiveTable[data[i]['grade']]);
+        if (data[i].major) {
+            grossMajorPoints += data[i].credit * fourPointFiveTable[data[i]['grade']];
         }
     }
+
     return [grossPoints, grossMajorPoints];
 }
 
-function gpaCalculator(data){
-let result = '';
-const credits = calculateCredits(data);
-const grossCredits = credits[0];
-const grossMajorCredits = credits[1];
+console.log(calculatePoints(data));
 
-const points = calculatePoints(data);
-const grossPoints = points[0];
-const grossMajorPoints = points[1];
 
-const gpaFourPointThree = (grossPoints / grossCredits).toFixed(2);
-const mgpaFourPointThree = (grossMajorPoints / grossMajorCredits).toFixed(2);
-const gpaFourPointFive = ((grossPoints / grossCredits)*(4.5/4.3)).toFixed(2);
-const mgpaFourPointFive = ((grossMajorPoints / grossMajorCredits)*(4.5/4.3)).toFixed(2);
+function gpaCalculator(data) {
+    let result = '';
+    const credits = calculateCredits(data);
+    const grossCredits = credits[0];
+    const grossMajorCredits = credits[1];
 
-result = '4.5 기준: 총 평점 = ' + gpaFourPointFive + ' 전공 평점 = ' + mgpaFourPointFive + '\n' +
-'4.3 기준: 총 평점 = ' + gpaFourPointThree + ' 전공 평점 = ' + mgpaFourPointThree + '\n' +
-'이수 학점: ' + grossCredits+ '\n' +
-'전공 이수 학점: ' + grossMajorCredits;
+    const points = calculatePoints(data);
+    const grossPoints = points[0];
+    const grossMajorPoints = points[1];
 
-return result;
+    const gpaFourPointThree = (grossPoints / grossCredits).toFixed(2);
+    const mgpaFourPointThree = (grossMajorPoints / grossMajorCredits).toFixed(2);
+    const gpaFourPointFive = ((grossPoints / grossCredits) * (4.5 / 4.3)).toFixed(2);
+    const mgpaFourPointFive = ((grossMajorPoints / grossMajorCredits) * (4.5 / 4.3)).toFixed(2);
+
+    result = '4.5 기준: 총 평점 = ' + gpaFourPointFive + ' 전공 평점 = ' + mgpaFourPointFive + '\n' +
+        '4.3 기준: 총 평점 = ' + gpaFourPointThree + ' 전공 평점 = ' + mgpaFourPointThree + '\n' +
+        '이수 학점: ' + grossCredits + '\n' +
+        '전공 이수 학점: ' + grossMajorCredits;
+
+    return result;
 }
 
 const result = gpaCalculator(data);
