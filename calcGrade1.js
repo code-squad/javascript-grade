ba42310f64d7fd209bafa72dbdd879b1fc1e14f6
Head <mrdoo309@gmail.com>
2018-09-19 15:55:44 +0900

    revise showGrade function using the functions created so far.

--- calcGrade1.js
+++ calcGrade1.js
@@ -20,15 +20,15 @@ var data = [
 ];
 //영어학점을 숫자로 변환
 const gradeScoreObject = {
-    'A+':4.5,
-    'A':4,
-    'B+':3.5,
-    'B':3,
-    'C+':2.5,
-    'C':2,
-    'D+':1.5,
-    'D':1,
-    'F':0
+    'A+': 4.5,
+    'A': 4,
+    'B+': 3.5,
+    'B': 3,
+    'C+': 2.5,
+    'C': 2,
+    'D+': 1.5,
+    'D': 1,
+    'F': 0
 }
 
 function getClassGrade(classData) {
@@ -48,17 +48,17 @@ function getClassCredit(classData) {
 function getMajorClassGrade(classData) {
     const majorClassGrade = []
     classData.forEach(classObject => {
-        if(classObject.major) {
+        if (classObject.major) {
             majorClassGrade.push(classObject.grade)
         }
     })
-    return majorClassGrade 
+    return majorClassGrade
 }
 
 function getMajorClassCredit(classData) {
     const majorClassCredit = []
     classData.forEach(classObject => {
-        if(classObject.major) {
+        if (classObject.major) {
             majorClassCredit.push(classObject.grade)
         }
     })
@@ -66,42 +66,32 @@ function getMajorClassCredit(classData) {
 }
 
 function getGradeAverage(classGrade, classCredit) {
-    const gradeScore = classGrade.map(gradeValue => {return gradeScoreObject[gradeValue]})
+    const gradeScore = classGrade.map(gradeValue => {
+        return gradeScoreObject[gradeValue]
+    })
     const multedGradeScore = classCredit.map((creditValue, index) => {
         return gradeScore[index] * classCredit[index]
     })
-    const sumOfGradeScore = multedGradeScore.reduce((beforeValue, CurrentValue) => {return beforeValue + CurrentValue})
-    const sumOfClassCredit = classCredit.reduce((beforeValue, CurrentValue) => {return beforeValue + CurrentValue})
+    const sumOfGradeScore = multedGradeScore.reduce((beforeValue, currentValue) => {
+        return beforeValue + currentValue
+    })
+    const sumOfClassCredit = classCredit.reduce((beforeValue, currentValue) => {
+        return beforeValue + currentValue
+    })
     return sumOfGradeScore / sumOfClassCredit
 }
 
 function showGrade(gradeData) {
-    let totalGrade = 0
-    let totalMajorGrade = 0
-    let totalCredit = 0
-    let totalMajorCredit = 0
-    
-    gradeData.forEach(classObject => {
-        for(classValue in classObject) {
-            const classCredit = classObject['credit']
-            const classGrade = gradeScoreObject[classObject['grade']] * classObject['credit']
-            if(classValue === 'grade') {
-                totalGrade = totalGrade + classGrade
-            } else if (classValue === 'credit') {
-                totalCredit = totalCredit + classCredit
-            } else if (classValue === 'major' && classObject['major']) {
-                totalMajorCredit = totalMajorCredit + classCredit
-                totalMajorGrade = totalMajorGrade + classGrade
-            }
-        }
-    });
-
-    const gradeAverage = calcGradeAverage(totalGrade, totalCredit, 4.5)
-    const majorGradeAverage = calcGradeAverage(totalMajorGrade, totalMajorCredit, 4.5)
-    const convertedGradeAverage = calcGradeAverage(totalGrade, totalCredit, 4.0)
-    const convertedMajorGradeAverage = calcGradeAverage(totalMajorGrade, totalMajorCredit, 4.0)
-    console.log('4.5기준 총평점 : ' + gradeAverage + '(4.0기준은 ' + convertedGradeAverage + ')');
-    console.log('4.5기준 전공평점 : ' + majorGradeAverage + '(4.0기준은 ' + convertedMajorGradeAverage + ')');
-    console.log('이수학점 : ' + totalCredit);
-    console.log('전공이수학점 : ' + totalMajorCredit);
+    const classGrade = getClassGrade(gradeData)
+    const classCredit = getClassCredit(gradeData)
+    const majorClassGrade = getMajorClassGrade(gradeData)
+    const majorClassCredit = getMajorClassCredit(gradaeData)
+    const gradeAverage = getGradeAverage(classGrade, classCredit)
+    const majorGradeAverage = getGradeAverage(majorClassGrade, majorClassCredit)
+    const sumOfCredit = classCredit.reduce((beforeValue, currentValue) => {
+        return beforeValue + currentValue;
+    })
+    const sumOfMajorCredit = majorClassCredit.reduce((beforeValue, currentValue) => {
+        return beforeValue + currentValue;
+    })
 }
