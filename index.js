b8180864f198c3ad9547c030e9a6974b97f5f49f
KaKa <sk2504@naver.com>
2018-09-19 16:47:19 +0900

    step1 학점계산기 구현 (#34)
    * step1 학점계산기 구현
    
    학점계산기를 구현했습니다, 4.5만점기준,4.0만점기준결과 출력및
    이수학점, 전공이수학점 표시기능도 추가.
    
    * 피드백 사항 수정
    
    화살표 함수로 코드길이를 줄이고, createMajorGradeObj 을filterlingMajorGradeObj 로 변경
    if 문에 불리언값 수정했습니다!
    
    * 파라미터 수정
    
    showgrade에 rating2 삭제

--- index.js
+++ index.js
@@ -1,60 +1,88 @@
-const readline = require('readline');
-
-var data = [];
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function convertGrade(currGrade) {
-    var grade = 'FDCBA';
-    var score = grade.indexOf(currGrade[0]);
-    if(grade[1] === '+' && score !== 0){
-        score += 0.5;
-    }
-    return score;
-}
-
-function calculateGradepoint(data) {
-    var creditSum = 0 , creditGradeSum = 0;
-    var majorCreditSum = 0 , majorCreditGradeSum = 0;
-    var gpa = 0,majorGpa = 0;
-    var convertGpa = 0;
-
-    data.forEach(function (curr) {
-        creditSum += curr.credit;
-        creditGradeSum += convertGrade(curr.grade)*curr.credit;
-        if(curr.bMajor){
-            if(curr.bMajor)
-            majorCreditSum += convertGrade(curr.grade);
-            majorCreditGradeSum += convertGrade(curr.grade)*curr.credit;
+// 김아무개의 학점데이터
+const gradesData = [
+    {
+        name: '데이터베이스',
+        grade: 'A',
+        credit: 3,
+        major: false,
+    },
+    {
+        name: '교양영어',
+        grade: 'B+',
+        credit: 2,
+        major: true,
+    },
+    {
+        name: '철학',
+        grade: 'B+',
+        credit: 1,
+        major: false,
+    },
+];
+// 4.5기준 학점등급 객체
+const fourPointFiveRating = {
+    'A+': 4.5,
+    'A': 4.0,
+    'B+': 3.5,
+    'B': 3.0,
+    'C+': 2.5,
+    'C': 2.0,
+    'D+': 1.5,
+    'D': 1.0,
+    'F': 0
+};
+// 학점데이터의 총 이수학점을 구하는 함수
+let getTotalOfCredits = function (gradesData) {
+    let totalOfCredits = 0;
+    gradesData.forEach(function (v) {
+        totalOfCredits += v.credit;
+    });
+    return totalOfCredits.toFixed(2);
+};
+// 성적을 계산하는 함수 (성적 * 이수학점)을 모두더한다
+let getTotlalOfGrades = function (gradesData, rating) {
+    let totlalOfGrades = 0;
+    gradesData.forEach(function (v) {
+        totlalOfGrades += (rating[v.grade] * v.credit);
+    });
+    return totlalOfGrades.toFixed(2);
+};
+// 4.0 기준 학점계산함수 (총학점 *4.0)/ 4.5
+let calculateFourPointZeroCredit = function (gradesData, rating) {
+    const FourPointZero = '4.0';
+    const Grades = calculateFourPointFiveCredit(gradesData, rating);
+    let fourPointzeroGrades = (Grades * FourPointZero) / rating['A+'];
+    return fourPointzeroGrades.toFixed(2);
+};
+// 4.5기준 학점계산함수
+let calculateFourPointFiveCredit = function (gradesData, rating) {
+    let fourPointFiveGrades = getTotlalOfGrades(gradesData, rating) / getTotalOfCredits(gradesData);
+    return fourPointFiveGrades;
+};
+// 전공과목 gradesData배열 생성함수
+let filterlingMajorGradeObj = function (gradesData) {
+    let majorGradesData = gradesData.filter(v => v.major === true);
+    return majorGradesData;
+};
+// 전공과목 이수학점 계산 함수
+let getTotalofMajorCredit = function (gradesData) {
+    let TotalofMajorCredit = 0;
+    gradesData.forEach(function (v) {
+        if (v.major) {
+            TotalofMajorCredit += v.credit;
         }
     });
-
-    gpa = (creditGradeSum/creditSum).toFixed(2);
-    majorGpa = (majorCreditGradeSum/majorCreditSum).toFixed(2);
-    convertGpa = ((creditGradeSum/creditSum) * 4 / 4.5).toFixed(2);
-
-    console.log('총평점 : ' + gpa);
-    console.log('전공평점 : ' + majorGpa);
-    console.log('이수학점 : ' + creditSum);
-    console.log('전공이수학점 : ' + majorCreditSum);
-    console.log('4.0으로 변환시 : ' + convertGpa);
-
+    return TotalofMajorCredit;
+};
+// 출력함수
+function showGrade(gradesData, rating) {
+    console.log('(4.0)기준 총 평점 : ' + calculateFourPointZeroCredit(gradesData, rating));
+    console.log('(4.5)기준 총 평점 : ' + calculateFourPointFiveCredit(gradesData, rating));
+    console.log('(4.0)기준 전공 학점 : ' + calculateFourPointZeroCredit(filterlingMajorGradeObj(gradesData), rating));
+    console.log('(4.5)기준 전공 학점 : ' + calculateFourPointFiveCredit(filterlingMajorGradeObj(gradesData), rating));
+    console.log('총 이수학점 : ' + getTotalOfCredits(gradesData));
+    console.log('총 전공이수학점 : ' + getTotalofMajorCredit(gradesData));
 }
 
-function addLecture() {
-    rl.question('과목을 JSON형태로 입력하세요<종료는 end입력>',function(answer){
-        if(answer === 'end'){
-            setTimeout(function () {
-                calculateGradepoint(data);
-            },2000);
-            return rl.close();
-        }
+showGrade(gradesData, fourPointFiveRating);
 
-        data.push(JSON.parse(answer));
-        addLecture();
-    });
-
-}addLecture();
