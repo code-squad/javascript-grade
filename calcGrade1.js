0e85f9d9011675ae52cb49bba9dccf0f2685d963
Head <mrdoo309@gmail.com>
2018-09-20 14:33:35 +0900

    find error and restart

--- calcGrade1.js
+++ calcGrade1.js
@@ -22,6 +22,12 @@ let data = [
         'grade': 'A+',
         'credit': 3,
         'major': true
+    },
+    {
+        'name': '백엔드마스터',
+        'grade': 'A+',
+        'credit': 2,
+        'major': false
     }
 ];
 //영어학점을 숫자로 변환
@@ -37,13 +43,14 @@ const gradeScoreObject = {
     'F': 0
 }
 
+const gradeArrays = 'A+,A,B+,B,C+,C,D+,D,F'.split(',')
 //1. 강의를 추가하는 함수
-function addLecture(className, classGrade, classCredit, isClassMajor) {
+function addLecture(className, classGrade, classCredit, classMajor) {
     var newLecture = {
         'name': className,
         'grade': classGrade,
         'credit': classCredit,
-        'major': isClassMajor
+        'major': classMajor
     }
     data.push(newLecture)
     showGrade(data)
@@ -53,7 +60,7 @@ function addLecture(className, classGrade, classCredit, isClassMajor) {
 function removeLecture(className, PrintTime) {
     let newData = data.filter(object => object.name !== className)
     data = newData
-    setTimeout( () => {
+    setTimeout(() => {
         showGrade(data)
     }, PrintTime)
 }
@@ -61,26 +68,27 @@ function removeLecture(className, PrintTime) {
 //3. 강의를 정렬해주는 함수
 //3-1. data 를 입력받아 학점을 정렬해주는 함수
 // sort이용해서 정렬
-function sortDataOrder(data) {
-    var sortedData = data.sort((beforeValue, value) =>{
-        if(gradeScoreObject[beforeValue.grade] > gradeScoreObject[value.grade]) {
+function sortCreditDataOrder(gradeData) {
+    var creditSortedData = gradeData.sort((beforeValue, value) => {
+        if (beforeValue.credit > value.credit) {
             return -1
-        } else if (gradeScoreObject[beforeValue.grade] < gradeScoreObject[value.grade]) {
+        } else if (beforeValue.credit < value.credit) {
             return 1
         }
     })
-    return sortedData
+    return creditSortedData
 }
-var a = sortDataOrder(data);
-console.log(a) 
-
+//문제가생김. => 성적으로 이루어진 배열을 credit으로 다시 정렬해야함
+//우선 배열을 만들고. sort는 credit에서만 실행하고
+//각각성적에따라 push. ->너무하드코딩 좋은방법이없을까
 function sortGrade(data) {
-    //정렬 후
-    //---------------부터출력
+    
     //forEach 이용해서 전부console
     //data[0].name, data[0].grade, data[0].credit
     //후------------출력
 }
+
+sortGrade(data)
 
 function getClassGrade(classData) {
     const classGrade = classData.map(classObject => {
@@ -141,7 +149,7 @@ function printResult(gradeAverage, majorGradeAverage, sumOfCredit, sumOfMajorCre
     console.log('총평점 : ' + gradeAverage + '(4.0 기준 : ' + convertGradeScore(gradeAverage) + ')');
     console.log('전공평점 : ' + majorGradeAverage + '(4.0기준 : ' + convertGradeScore(gradeAverage) + ')');
     console.log('이수학점 : ' + sumOfCredit)
-    console.log('전공이수학점 : ' + sumOfMajorCredit) 
+    console.log('전공이수학점 : ' + sumOfMajorCredit)
 }
 
 function showGrade(gradeData) {
@@ -149,7 +157,7 @@ function showGrade(gradeData) {
     const classCredit = getClassCredit(gradeData)
     const majorClassGrade = getMajorClassGrade(gradeData)
     const majorClassCredit = getMajorClassCredit(gradeData)
-    if(majorClassGrade[0] === undefined) {
+    if (majorClassGrade[0] === undefined) {
         console.log('전공수업을 듣지않았으니 다시 듣고오세요')
         return;
     }
