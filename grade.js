3e221d839c5038801e3bcb8871f8567924cf701b
cohily12 <k96079123@gmail.com>
2018-09-21 10:48:05 +0900

    Lec2-Step2 (#33)
    * add addLecture & removeLecture func
    
    * add sortMyGrade func
    
    * modify return value
    
    * modify sortMyGrade func for printing like it would be

--- grade.js
+++ grade.js
@@ -1,30 +1,78 @@
-//data의 값은 사용자에 따라 다를 수 있으므로 var로 할당
-var data = [{'name' : '데이터베이스', 'grade' : 'A', 'credit' : 3, 'major' : false},
-                {'name' : '교양영어', 'grade' : 'B+', 'credit' : 2, 'major' : true},
-                {'name' : '철학', 'grade' : 'B+', 'credit' : 1, 'major' : false}];
+const data = [{'name' : '데이터베이스', 'grade' : 'A', 'credit' : 3, 'major' : false},
+            {'name' : 'Java 완전정복', 'grade' : 'D', 'credit' : 3, 'major' : true},
+            {'name' : '프로그래밍 설계', 'grade' : 'B', 'credit' : 2, 'major' : false},
+            {'name' : '네트워크실습', 'grade' : 'A', 'credit' : 1, 'major' : true},
+            {'name' : '자료구조와 알고리즘', 'grade' : 'B', 'credit' : 3, 'major' : false},
+            {'name' : 'VIM으로 최강속도 코딩하기', 'grade' : 'D', 'credit' : 1, 'major' : true},
+            {'name' : '웹프로그래밍', 'grade' : 'A', 'credit' : 3, 'major' : false},
+            {'name' : '이산수학', 'grade' : 'B', 'credit' : 3, 'major' : true}];            
+
+const gradeData = {'A+': 4.5, 'A' : 4.0, 'B+': 3.5, 'B' : 3.0, 'C+': 2.5, 'C' : 2.0, 'D+': 1.5, 'D' : 1.0, 'F' : 0};            
 
 function showGrade(data){
-
-    const gradeData = {'A+': 4.5, 'A' : 4.0, 'B+': 3.5, 'B' : 3.0, 'C+': 2.5, 'C' : 2.0, 'D+': 1.5, 'D' : 1.0, 'F' : 0};
     const majorSubject = (data => data.filter(val => val.major))(data);
-
+    
     function getCredit(data){
-        const credit = data.map(val => val.credit);
-        return credit.reduce((a,b) => a+b);
+        const creditArr = data.map(val => val.credit);
+        const sumCredit = creditArr.reduce((a,b) => a+b);
+        return sumCredit;
     }
 
     function getGPA(data, grade){
         const multiCreditGrade = data.map(val => val.credit * grade[val.grade]);
-        return (multiCreditGrade.reduce((a,b) => a+b) / getCredit(data));           
+        const gpa = multiCreditGrade.reduce((a,b) => a+b) / getCredit(data); 
+        return gpa;           
     }
 
     function convertGPA(gpa, scoreStandard){
         return scoreStandard === 4.5 ? gpa.toFixed(2) : (scoreStandard * gpa / 4.5).toFixed(2);
-    }
-
+    } 
     console.log(`4.5기준 총평점 : ${convertGPA(getGPA(data, gradeData), 4.5)}(4.0기준은 ${convertGPA(getGPA(data, gradeData), 4.0)}) 
         전공평점 : ${convertGPA(getGPA(majorSubject, gradeData), 4.5)}(4.0기준은 ${convertGPA(getGPA(majorSubject, gradeData), 4.0)}) 
         이수학점 : ${getCredit(data)} 
         전공이수학점 : ${getCredit(majorSubject)}`);
-
 }
+
+function addLecture(lectureData){
+    data.push(lectureData);
+    showGrade(data);
+}
+
+function removeLecture(lectureName, time){
+    data.forEach((lecture, idx) => {
+        if(lecture.name === lectureName) data.splice(idx, 1);
+    }); 
+    setTimeout(showGrade, time, data);
+}
+
+function sortMyGrade(data){
+    const classifyByGrade = {};
+    const gradeDataKeys = Object.keys(gradeData);
+    
+    for(let key of gradeDataKeys){
+        classifyByGrade[key] = data.filter(lec => lec.grade === key);
+    }
+    for(let lec in classifyByGrade){
+        classifyByGrade[lec].sort((lecture1, lecture2) => lecture2.credit-lecture1.credit);                
+    }
+        
+    console.log(`-------------${stringfyData(classifyByGrade)}-------------`);
+}
+
+function stringfyData(data){
+    let stringData = '';
+    for(let grade in data){
+        if(data[grade].length){
+            stringData += data[grade].reduce((lec1, lec2) => lec1 + `${lec2.name}, ${lec2.grade}, ${lec2.credit}학점\n`, '\n');
+        }
+    }
+    return stringData;
+}
+
+/*
+console.log(data);
+showGrade(data);
+addLecture({'name' : '알고리즘', 'grade' : 'B+', 'credit' : 3, 'major' : true});
+removeLecture('알고리즘', 1000);
+sortMyGrade(data);
+*/
