90d658db5dea5bbc7bc5e8e0aa961e07c3fa7156
Head <mrdoo309@gmail.com>
2018-09-23 14:08:44 +0900

    sortGrade함수를 위한 divideByGrade함수를 만들고 printSortedData함수를 수정함

--- cleanCalcGrade.js
+++ cleanCalcGrade.js
@@ -57,19 +57,17 @@ function getGradeAverage(gradeArrays, creditArrays) {
     return (summedScore / summedCredit).toFixed(2)
 }
 
-//점수값만큼 학점을 곱해주는 함수 
+//학점평균을 위해 점수값을 학점값만큼 곱해주는 함수 
 function multScoreArrays(gradeArrays, creditArrays) {
     const scoreObject = { 'A+': 4.5, 'A': 4, 'B+': 3.5, 'B': 3, 'C+': 2.5, 'C': 2, 'D+': 1.5, 'D': 1, 'F': 0 }
     const scoreArrays = gradeArrays.map(gradeValue => scoreObject[gradeValue])
-    return creditArrays.map((creditValue, index) => {
-        return scoreArrays[index] * creditArrays[index]
-    })
+    return creditArrays.map((creditValue, index) => scoreArrays[index] * creditArrays[index])
 }
 
 //배열값을 더해주는 함수
 function sumArrays(arrays) {
     return arrays.reduce((beforeVal, currentVal) => {
-        return beforeVal + currentVal 
+        return beforeVal + currentVal
     }, 0)
 }
 
@@ -78,21 +76,21 @@ function convertGradeScore(score) {
     return (score / 4.5 * 4.0).toFixed(2)
 }
 
+//총평점과 전공평점, 이수학점, 전공이수학점을 계산해주는 함수
+function showGrade(gradeData) {
+    const gradeAverage = getGradeAverage(getGradeArrays(gradeData), getCreditArrays(gradeData)),
+    majorGradeAverage = getGradeAverage(getMajorGradeArrays(gradeData), getMajorCreditArrays(gradeData)),
+    creditLoad = sumArrays(getCreditArrays(gradeData)),
+    majorCreditLoad = sumArrays(getMajorCreditArrays(gradeData))
+    printResult(gradeAverage, majorGradeAverage, creditLoad, majorCreditLoad)
+}
+
 //결과를 출력해주는 함수
 function printResult(gradeAverage, majorGradeAverage, creditLoad, majorCreditLoad) {
     console.log(`총평점 : ${gradeAverage}(4.0 기준 : ${convertGradeScore(gradeAverage)})`);
     console.log(`전공평점 : ${majorGradeAverage}(4.0기준 : ${convertGradeScore(majorGradeAverage)})`);
     console.log(`이수학점 : ${creditLoad}`);
     console.log(`전공이수학점 : ${majorCreditLoad}`);
-}
-
-//총평점과 전공평점, 이수학점, 전공이수학점을 계산해주는 함수
-function showGrade(gradeData) {
-    const gradeAverage = getGradeAverage(getGradeArrays(gradeData), getCreditArrays(gradeData)),
-        majorGradeAverage = getGradeAverage(getMajorGradeArrays(gradeData), getMajorCreditArrays(gradeData)),
-        creditLoad = sumArrays(getCreditArrays(gradeData)),
-        majorCreditLoad = sumArrays(getMajorCreditArrays(gradeData))
-    printResult(gradeAverage, majorGradeAverage, creditLoad, majorCreditLoad)
 }
 
 //강의를 추가하는 함수 
@@ -108,27 +106,23 @@ function removeLecture(className, printTime) {
     setTimeout(() => showGrade(data), printTime)
 }
 
-//정렬해주는 함수
+//정렬하는 함수
 function sortGrade(classData) {
-    const gradeArrays = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F']
     console.log(`--------------------`)
     console.log(``)
-    gradeArrays.forEach(value => {
-        sortGradeOrder(classData, value)
-    })
+    divideByGrade(classData)
     console.log(`--------------------`)
 }
 
-//성적순으로 정렬해주는 함수
-function sortGradeOrder(classData, gradeValue) {
-    let oneGradeData = classData.filter(object => gradeValue === object.grade)
-    if (oneGradeData.length === 0) return;
-    let sortedArray = sortCreditOrder(oneGradeData)
-    sortedArray.forEach(sortedObject => {
-        let { name, grade, credit } = sortedObject
-        printSortedData(name, grade, credit)
+//학점별로 나누어 주는 함수
+function divideByGrade(classData) {
+    const gradeArrays = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F']
+    gradeArrays.forEach(value => {
+        let oneGradeData = classData.filter(object => value === object.grade)
+        if(oneGradeData.length === 0) return;
+        printSortedData(sortCreditOrder(oneGradeData))
+        console.log('')
     })
-    console.log(``)
 }
 
 //학점순으로 정렬해주는 함수
@@ -141,6 +135,9 @@ function sortCreditOrder(gradeData) {
 }
 
 //정렬된 배열을 출력해주는 함수
-function printSortedData(name, grade, credit) {
-    console.log(`${name}, ${grade}, ${credit}학점`)
+function printSortedData(sortedArray) {
+    sortedArray.forEach(sortedObject => {
+        let { name, grade, credit } = sortedObject
+        console.log(`${name}, ${grade}, ${credit}학점`)
+    })
 }
