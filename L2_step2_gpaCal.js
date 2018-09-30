3f129dfd09608cb2f07218cfce27537a8557e8b9
Soom <40848630+developersoom@users.noreply.github.com>
2018-09-30 20:45:22 +0900

    [숨] 학점계산기_step 2 (#45)
    * Move fourPointFiveTable into global scope
    
    * Modify getValues() with destructuring
    
    still look ugly with variable 'elements'....
    
    * Modify gpaCalculator() with template
    
    * Add addLecture()
    
    * Delete 4.5 result in gpaCalculator()
    
    * Add removeLecture()
    
    + change addLecture() return value
    
    * Modify 'data' -> 'dataArr'
    
    * Add sortGrade() - not perfect
    
    'A' prints earlier than 'A+'..
    
    * Add sortGrade(), print()
    
    * Edit file name
    
    * Edit file name
    
    * Edit removeLecture() with filter
    
    * Edit sortGrade()
    
    * Remove getValues()
    
    * Edit sortGrade() with forEach()
    
    * Edit gpaCalculator()
    
    * Edit gpaCalculator()
    
    - no calculation in template
    
    * Edit sortGrade() without using for in for

---
+++ L2_step2_gpaCal.js
@@ -0,0 +1,128 @@
+let data = [{
+        'name': '데이터베이스',
+        'grade': 'A',
+        'credit': 3,
+        'major': false
+    },
+    {
+        'name': '교양영어',
+        'grade': 'B',
+        'credit': 3,
+        'major': true
+    },
+    {
+        'name': '철학',
+        'grade': 'B',
+        'credit': 2,
+        'major': false
+    },
+    {
+        'name': '경영',
+        'grade': 'A+',
+        'credit': 1,
+        'major': true
+    },
+    {
+        'name': '통계',
+        'grade': 'A+',
+        'credit': 2,
+        'major': true
+    },
+];
+
+const fourPointFiveTable = {
+    'A+': 4.5,
+    'A': 4.0,
+    'B+': 3.5,
+    'B': 3.0,
+    'C+': 2.5,
+    'C': 2.0,
+    'D+': 1.5,
+    'D': 1.0,
+    'F': 0,
+}
+
+function addLecture(newLecture) {
+    const newData = data;
+    newData.push(newLecture);
+    const result = gpaCalculator(newData, 4.5);
+    console.log(result);
+}
+
+function removeLecture(dataArr, lecture, time) {
+    setTimeout(function () {
+        const newData = dataArr.filter(function (value) {
+            return value.name !== lecture;
+        })
+
+        data = newData;
+        const result = gpaCalculator(newData, 4.5);
+        console.log(result);
+    }, time);
+}
+
+function calculateCredits(dataArr) {
+    let grossCredits = 0;
+    let grossMajorCredits = 0;
+    for (const key in dataArr) {
+        grossCredits += dataArr[key].credit;
+        if (dataArr[key].major) {
+            grossMajorCredits += dataArr[key].credit;
+        }
+    }
+    return [grossCredits, grossMajorCredits];
+}
+
+function calculatePoints(dataArr) {
+    let grossPoints = 0;
+    let grossMajorPoints = 0;
+
+    for (const i in dataArr) {
+        grossPoints += (dataArr[i].credit * fourPointFiveTable[dataArr[i]['grade']]);
+        if (dataArr[i].major) {
+            grossMajorPoints += dataArr[i].credit * fourPointFiveTable[dataArr[i]['grade']];
+        }
+    }
+
+    return [grossPoints, grossMajorPoints];
+}
+
+function gpaCalculator(dataArr, scale) {
+    const [grossCredits, majorCredits] = calculateCredits(dataArr);
+    const [grossPoints, majorPoints] = calculatePoints(dataArr);
+    const gpa = ((grossPoints / grossCredits) * (scale / 4.5)).toFixed(2);
+    const mgpa = ((majorPoints / majorCredits) * (scale / 4.5)).toFixed(2);
+
+    const template =
+        `
+    ${scale} 기준: 총 평점 = ${gpa}, 전공 평점 = ${mgpa}
+    이수 학점: ${grossCredits}
+    전공 이수 학점: ${majorCredits}`
+
+    return template;
+}
+
+function sortGrade(dataArr) {
+    dataArr.sort(function (a, b) {
+        return fourPointFiveTable[b.grade] - fourPointFiveTable[a.grade];
+    });
+
+    dataArr.sort(function (a, b) {
+        if (a.grade === b.grade) {
+            return b.credit - a.credit;
+        }
+    });
+
+    return dataArr;
+}
+
+function print(dataArr) {
+    let data = sortGrade(dataArr);
+    let output = "";
+    output += `-----------------------------------------\n`
+    for (i = 0; i < data.length; i++) {
+        output += `${data[i]['name']}, ${data[i]['grade']}, ${data[i]['credit']}학점\n`
+    }
+    output += `-----------------------------------------`;
+    return output;
+}
