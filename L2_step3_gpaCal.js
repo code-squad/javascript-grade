7704ad01c8407ded40791379ba30ab0c7b73e3b5
Soom <40848630+developersoom@users.noreply.github.com>
2018-10-02 16:55:59 +0900

    [숨] 학점계산기_step 3 (#48)
    * Move fourPointFiveTable into global scope
    
    * Modify getValues() with destructuring
    
    still look ugly with variable 'elements'....
    
    * Modify gpaCalculator() with template
    
    * Add addLecture()
    
    * Delete 4.5 result in gpaCalculator()
    
    * Add removeLecture()
    
    + change addLecture() return value
    
    * Modify 'data' -> 'dataArr'
    
    * Add sortGrade() - not perfect
    
    'A' prints earlier than 'A+'..
    
    * Add sortGrade(), print()
    
    * Edit file name
    
    * Edit file name
    
    * Edit removeLecture() with filter
    
    * Edit sortGrade()
    
    * Remove getValues()
    
    * Edit sortGrade() with forEach()
    
    * Edit gpaCalculator()
    
    * Edit gpaCalculator()
    
    - no calculation in template
    
    * Edit sortGrade() without using for in for
    
    * Change to arrow function
    
    * Change function name to verb
    
    * Change for-loop to forEach() in printResult()
    
    * Add calculateGpa()
    
    * Edit addLecture() deleting variable 'newData'
    
    * Fix git errors
    
    * Add file
    
    * Fix errors
    
    * Change setTimeout() with  arrow function in removeLecture
    
    * Replace for-in with for-of in calculatePoints()
    
    * Replace for-in with for-of in calculateCredits()
    
    * Replace '+=' with 'reduce' in printResult()
    
    * Edit printResult()
    
    * Edit file name from step2 to step3

---
+++ L2_step3_gpaCal.js
@@ -0,0 +1,126 @@
+let data = [{
+        'name': '데이터베이스',
+        'grade': 'A',
+        'credit': 3,
+        'major': false
+    },
+    {
+        'name': '교양영어',
+        'grade': 'B',
+        'credit': 3,
+        'major': true
+    },
+    {
+        'name': '철학',
+        'grade': 'B',
+        'credit': 2,
+        'major': false
+    },
+    {
+        'name': '경영',
+        'grade': 'A+',
+        'credit': 1,
+        'major': true
+    },
+    {
+        'name': '통계',
+        'grade': 'A+',
+        'credit': 2,
+        'major': true
+    },
+];
+
+const fourPointFiveTable = {
+    'A+': 4.5,
+    'A': 4.0,
+    'B+': 3.5,
+    'B': 3.0,
+    'C+': 2.5,
+    'C': 2.0,
+    'D+': 1.5,
+    'D': 1.0,
+    'F': 0,
+}
+
+const addLecture = (newLecture) => {
+    data.push(newLecture);
+    const result = getResult(data, 4.5);
+    console.log(result);
+}
+
+const removeLecture = (dataArr, lecture, time) => {
+    setTimeout(() => {
+        dataArr.filter(value => value.name !== lecture);
+        const result = getResult(dataArr, 4.5);
+        console.log(result);
+    }, time);
+}
+
+const calculateCredits = (dataArr) => {
+    let grossCredits = 0;
+    let grossMajorCredits = 0;
+    for (const key of dataArr) {
+        grossCredits += key.credit;
+        if (key.major) {
+            grossMajorCredits += key.credit;
+        }
+    }
+    return [grossCredits, grossMajorCredits];
+}
+
+const calculatePoints = (dataArr) => {
+    let grossPoints = 0;
+    let grossMajorPoints = 0;
+    for (const value of dataArr) {
+        grossPoints += (value.credit * fourPointFiveTable[value.grade]);
+        if (value.major) {
+            grossMajorPoints += value.credit * fourPointFiveTable[value.grade];
+        }
+    }
+    return [grossPoints, grossMajorPoints];
+}
+
+const calculateGpa = (dataArr, scale) => {
+    const points = calculatePoints(dataArr);
+    const credits = calculateCredits(dataArr);
+    const gpa = ((points[0] / credits[0]) * (scale / 4.5)).toFixed(2);
+    const mgpa = ((points[1] / credits[1]) * (scale / 4.5)).toFixed(2);
+    return [gpa, mgpa];
+}
+
+const getResult = (dataArr, scale) => {
+    const [grossCredits, majorCredits] = calculateCredits(dataArr);
+    const [gpa, mgpa] = calculateGpa(dataArr, 4.5);
+
+    const template =
+        `
+${scale} 기준: 총 평점 = ${gpa}, 전공 평점 = ${mgpa}
+이수 학점: ${grossCredits}
+전공 이수 학점: ${majorCredits}`;
+
+    return template;
+}
+
+const sortGrade = (dataArr) => {
+    dataArr.sort(function (a, b) {
+        return fourPointFiveTable[b.grade] - fourPointFiveTable[a.grade];
+    });
+
+    dataArr.sort(function (a, b) {
+        if (a.grade === b.grade) {
+            return b.credit - a.credit;
+        }
+    });
+
+    return dataArr;
+}
+
+const printResult = (dataArr) => {
+    let data = sortGrade(dataArr);
+    let result = data.reduce(function (accumulator, currentValue) {
+        return accumulator + `${currentValue.name}, ${currentValue.grade}, ${currentValue.credit}학점\n`
+    }, "");
+    return result;
+}
+
+console.log(`---------------------------------------- \n${printResult(data)}----------------------------------------`);
